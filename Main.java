a46ed1d75c52b5c608cfcc50e456149aa89aaac6
MinT <AndrewTrinhHuynh@outlook.com>
2026-03-04 11:47:07 +0800

    finished section 5 of arithmetic operators and stuff

---
+++ Main.java
@@ -0,0 +1,35 @@
+
+public class Main{
+    
+    public static void main (String[] args){
+
+        //Arithmetic Operators
+
+        //int x = 10;
+        //int y = 3;
+        //int z;
+
+        //z = x + y;
+        //z = x - y;
+        //z = x * y;
+        //z = x / y;
+        //z = x % y;
+        //System.out.println(z)
+
+        //Augmented assignment operatores
+        //x = x + y; - instead of doing this to reassign the value of x 
+        //x += y; - do this, applies to all below 
+        //x *= y;
+        //x /= y;
+        //x %= y;
+
+        //Increment and Decrement Operators
+        // x++ (just like a for loop where int i = 0; i < x; i++)
+        // x--
+
+        //Order of Operators: PEMDAS (parenthesis, exponentials, multiplication, division, addition and subtraction)
+
+        double result = 3 + 4 * (7 - 5) / 2.0;
+        System.out.println(result);
+    }
+}
